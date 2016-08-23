ab72259377f5fc5cd7d37c1888a4ecf054b2111d
changi <changi@is.com.tw>
2016-08-23 15:39:30 +0800

    add headers setting

--- usingUrllib.py
+++ usingUrllib.py
@@ -88,7 +88,7 @@ class urltoolslib(object):
 	def __init__(self, url):
 		self._request_headers = {
 			"Accept-Language": "en-US,en;q=0.5",
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0 ms/1.0",
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
 			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 			"Connection": "keep-alive" 
 		}
